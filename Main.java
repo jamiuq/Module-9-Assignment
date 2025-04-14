e64217b2c8a7ee29ad15d8d536a7373d03b3267f
jamiuq <jamiuq@farmingdale.edu>
2025-04-14 00:10:02 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main
+{
+  public static void main(String[] args)
+  {
+    Rectangle shape1 = new Rectangle(6, 7); // creates object of rectangle class
+    Square shape2 = new Square(8); // creates object of square class, which inherits from rectangle class
+    Rectangle shape3 = new Rectangle(1, 4);
+
+    shapeCollection group = new shapeCollection(); // creates shapeCollection class, which composes of objects of the Rectangle class
+    group.addShape(shape1); group.addShape(shape2); group.addShape(shape3);
+
+    System.out.println(group.getSumOfShapeAreas());
+  }
+}
