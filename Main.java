eb815b499aa12973fec20adeccbae2c1f613106c
jamiuq <jamiuq@farmingdale.edu>
2025-04-14 00:14:14 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ public class Main
     Square shape2 = new Square(8); // creates object of square class, which inherits from rectangle class
     Rectangle shape3 = new Rectangle(1, 4);
 
+    shape1.printInfo();
+    shape2.printInfo(); // square class's printInfo overrides the rectangle class's
+
     shapeCollection group = new shapeCollection(); // creates shapeCollection class, which composes of objects of the Rectangle class
     group.addShape(shape1); group.addShape(shape2); group.addShape(shape3);
 
