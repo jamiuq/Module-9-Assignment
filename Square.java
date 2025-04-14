dcc5fba0d92f6a5cc17590c5fae977089479bf27
jamiuq <jamiuq@farmingdale.edu>
2025-04-14 00:13:07 -0400

    Update Square.java

--- Square.java
+++ Square.java
@@ -3,4 +3,10 @@ import java.util.*;
 public class Square extends Rectangle
 {
   public Square(int length) {super(length, length);}
+
+  @Override
+  public void printInfo()
+  {
+    System.out.println("Shape: Square\nLength: " + super.getLength + "\nArea: " + super.getArea() + "\n");
+  }
 }
